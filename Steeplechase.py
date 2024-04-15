8e336b6080e7834836d8286fc073c13375a77a47
Austinechen <167058333+Austinechen@users.noreply.github.com>
2024-04-15 15:09:58 +0800

    Update Steeplechase.py

---
+++ Steeplechase.py
@@ -0,0 +1,68 @@
+"""
+File: Steeplechase.py
+Name: Austine:
+---------------------------------
+Austine:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel crosses hurdles in a 12x12 world
+    with a for loop 
+    """
+    for i in range(11):
+        if front_is_clear():
+            move()
+        else:
+            jump()
+            turn_left()
+
+
+def jump():
+    """
+    pre-condition:Karel is on the left side of the wall,facing East
+    post-condition:Karel is facing North
+    """
+    up()
+    cross()
+    down()
+
+
+def up():
+    turn_left()
+    while not right_is_clear():
+        move()
+    """
+    while not front_is_clear():
+        turn_left()
+        move()
+        turn_right()
+    """
+
+
+def cross():
+    """
+    pre-condition:Karel is on the top of the hurdle,facing North
+    post-condition:Karel is at the upper right,facing South
+    """
+    turn_right()
+    move()
+    turn_right()
+
+
+def down():
+    while front_is_clear():
+        move()
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+
+# ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
